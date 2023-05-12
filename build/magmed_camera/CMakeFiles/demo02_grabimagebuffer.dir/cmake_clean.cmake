138bceb81f05158bfc2a575912a5bfeab14c78f9
WayneWu0526 <17850828526@163.com>
2023-05-12 12:57:00 +0800

    Add BGR callback

---
+++ build/magmed_camera/CMakeFiles/demo02_grabimagebuffer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/zhang/magmed_ws/devel/lib/magmed_camera/demo02_grabimagebuffer"
+  "/home/zhang/magmed_ws/devel/lib/magmed_camera/demo02_grabimagebuffer.pdb"
+  "CMakeFiles/demo02_grabimagebuffer.dir/src/demo02_grabimagebuffer.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo02_grabimagebuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
