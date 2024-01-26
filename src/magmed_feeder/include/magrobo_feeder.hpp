29e47bade934876836c0e1d9b75a99248f998222
WayneWu0526 <zhiweiwu.cn@outlook.com>
2024-01-26 23:42:44 +0800

    Update servo

--- src/magmed_feeder/include/magrobo_feeder.hpp
+++ src/magmed_feeder/include/magrobo_feeder.hpp
@@ -50,9 +50,13 @@ private:
     ros::Subscriber joystick_sub;
     serial::Serial feeder_serial;
     Joystick joystick;
-    // crc_modebus check
+    // write single
     int writeModbus(unsigned char addr, uint16_t reg, uint16_t value);
+    // write multiple
+    int writeModbus_ex(unsigned char addr, std::vector<uint16_t>& reg_values);
+    // read modbus
     int readModbus(unsigned char addr, uint16_t reg, uint16_t regNum);
+    // crc_modebus check
     uint16_t crc16_modbus(uint8_t *data, uint16_t len);
 };
 
@@ -76,23 +80,48 @@ void Servo::run()
         ros::shutdown();
         return;
     }
-    // 3. read MODBUS from the servo
-    nRet = readModbus(0x01, 0x0042, 0x0001);
+
+    // 2. 设置
+    nRet = readModbus(0x01, 0x003e, 0x0001);
     if (nRet < 0)
     {
         ros::shutdown();
         return;
     }
 
+    // 3. 设置上电
+    nRet = writeModbus(0x01, 0x00b2, 0x0001);
+    if (nRet < 0)
+    {
+        ros::shutdown();
+        return;
+    }
     while(ros::ok())
     {
         // 3. write MODBUS to the servo
         // printf("VEL: 0x%x\n", joystick.VEL);
-        // writeModbus(0x01, 0x0042, joystick.VEL);
+        nRet = writeModbus(0x01, 0x0042, 0x002); // 0x4e20-> 20000
+        if (nRet < 0)
+        {
+            printf("Write data failed.\n");
+            ros::shutdown();
+            break;
+        }
         ros::spinOnce();
         loop_rete.sleep();
     }
-
+    nRet = writeModbus(0x01, 0x0042, 0x0000);
+    if (nRet < 0)
+    {
+        printf("Write data failed.\n");
+    }
+    // 3. 设置下电
+    nRet = writeModbus(0x01, 0x00b2, 0x0000);
+    if (nRet < 0)
+    {
+        ros::shutdown();
+        return;
+    }
     // 4. Close the serial port
     nRet = closeSerialPort();
     if (nRet < 0)
@@ -111,6 +140,10 @@ int Servo::activateVelMode()
     {
         printf("Activate velocity mode failed.\n");
         return -1;
+    }
+    else
+    {
+        printf("Activate velocity mode successfully.\n");
     }
     return 0;
 }
@@ -167,11 +200,12 @@ int Servo::writeModbus(unsigned char addr, uint16_t reg, uint16_t value)
     data[6] = crc & 0xFF;
     data[7] = crc >> 8;
     // print the data
-    for (int i = 0; i < 8; ++i)
-    {
-        printf("0x%x ", data[i]);
-    }
-    printf("\n");
+    // printf("set: ");
+    // for (int i = 0; i < 8; ++i)
+    // {
+    //     printf("0x%x ", data[i]);
+    // }
+    // printf("\n");
     // try-catch to write data to the serial port
     try
     {
@@ -182,6 +216,88 @@ int Servo::writeModbus(unsigned char addr, uint16_t reg, uint16_t value)
         ROS_ERROR_STREAM("Unable to write data to the serial port.");
         return -1;
     }
+
+    usleep(1000 * TIMEOUT);
+    size_t bytes_available = feeder_serial.available();
+    if(bytes_available == 0)
+    {
+        printf("No data available.\n");
+        return -1;
+    }
+    uint8_t response[1024] = {0};
+    size_t bytes_read = feeder_serial.read(response, sizeof(response));
+    std::vector<uint8_t> packet(response, response + bytes_read);
+    // 打印 packet 中的内容
+    // printf("response: ");
+    // for (size_t i = 0; i < packet.size(); ++i)
+    // {
+    //     printf("0x%x ", packet[i]);
+    // }
+    // printf("\n");
+    if(data[4] == packet[4] && data[5] == packet[5])
+    {
+        // printf("Write data successfully.\n");
+    }
+    else
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int Servo::writeModbus_ex(unsigned char addr, std::vector<uint16_t>& reg_values)
+{
+    // 计算数据长度
+    uint16_t data_length = static_cast<uint16_t>(reg_values.size() * 2); // 每个寄存器两个字节
+
+    // 准备数据缓冲区
+    std::vector<uint8_t> data;
+    data.push_back(addr);           // 从机地址
+    data.push_back(0x10);           // 功能码
+    data.push_back(0x00);           // 寄存器地址高位
+    data.push_back(0x00);           // 寄存器地址低位
+    data.push_back(data_length >> 8);  // 寄存器数量高位
+    data.push_back(data_length & 0xFF); // 寄存器数量低位
+
+    // 添加寄存器地址和对应的数值
+    for (const uint16_t& reg_value : reg_values)
+    {
+        data.push_back(reg_value >> 8);  // 寄存器值高位
+        data.push_back(reg_value & 0xFF); // 寄存器值低位
+    }
+
+    // 计算CRC校验
+    uint16_t crc = crc16_modbus(data.data(), static_cast<uint16_t>(data.size()));
+    data.push_back(crc & 0xFF);      // CRC校验低位
+    data.push_back(crc >> 8);       // CRC校验高位
+
+    // 发送数据
+    try
+    {
+        feeder_serial.write(data.data(), data.size());
+    }
+    catch (serial::IOException& e)
+    {
+        ROS_ERROR_STREAM("Unable to write data to the serial port.");
+    }
+
+    usleep(1000 * TIMEOUT);
+    size_t bytes_available = feeder_serial.available();
+    if(bytes_available == 0)
+    {
+        printf("No data available.\n");
+        return -1;
+    }
+    uint8_t response[1024] = {0};
+    size_t bytes_read = feeder_serial.read(response, sizeof(response));
+    std::vector<uint8_t> packet(response, response + bytes_read);
+    // 打印 packet 中的内容
+    printf("response: ");
+    for (size_t i = 0; i < packet.size(); ++i)
+    {
+        printf("0x%x ", packet[i]);
+    }
+    printf("\n");
 
     return 0;
 }
@@ -207,7 +323,7 @@ int Servo::readModbus(unsigned char addr, uint16_t reg, uint16_t regNum)
     }
     printf("\n");
     // try-catch to write data to the serial port
-    feeder_serial.flush();
+    // feeder_serial.flush();
     try
     {
         feeder_serial.write(data, 8);
@@ -218,7 +334,7 @@ int Servo::readModbus(unsigned char addr, uint16_t reg, uint16_t regNum)
         return -1;
     }
 
-    usleep(500000);
+    usleep(1000 * TIMEOUT);
     size_t bytes_available = feeder_serial.available();
     if(bytes_available == 0)
     {
@@ -266,7 +382,7 @@ void Joystick::feed(magmed_msgs::PFjoystickConstPtr pMsg)
     // 1. Open the serial port
     joystick = *pMsg;
     // 数据高位
-    VEL_GAIN = joystick.POTA * 3;
+    VEL_GAIN = joystick.POTA * 20;
     VEL = static_cast<uint16_t>(VEL_GAIN * joystick.nJOY3[0]);
     // 数据低位
 }
